75f3fa83ed7429e94d04f096e8ea947d7c5bdbef
Vamdev Ravindran <vamdev@Vamdevs-MacBook-Pro.local>
2025-03-25 21:53:07 +0530

    changes

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>R.C.C. SPICES</h1>
+                <p>The Gold Standard in Cardamom – Trusted Quality, Unmatched Purity.</p>
             </div>
         </div>
         <nav>
